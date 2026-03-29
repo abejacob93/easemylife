5efb3ba8b9a16ecffa6a9d10e2c20aa205030983
abejacob93 <abejacob93@gmail.com>
2026-03-29 23:10:34 +0700

    Update create-order.js

--- .netlify/functions/create-order.js
+++ .netlify/functions/create-order.js
@@ -1,6 +1,31 @@
 const Razorpay = require("razorpay");
 
-const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET
-});
+exports.handler = async (event) => {
+  try {
+    const razorpay = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET
+    });
+
+    const { amount } = JSON.parse(event.body);
+
+    const order = await razorpay.orders.create({
+      amount: amount * 100,
+      currency: "INR"
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        order: order,
+        key: process.env.RAZORPAY_KEY_ID   // 👈 VERY IMPORTANT
+      })
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
